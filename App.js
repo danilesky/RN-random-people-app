b9e1c00eae82053fbbc751018083b1d8d7b72ac1
Daniel Skysľak <danilesky17@gmail.com>
2022-09-03 16:38:54 +0200

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello Simi 3</Text>
+      <Text>My Love</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +17,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'column'
   },
 });
